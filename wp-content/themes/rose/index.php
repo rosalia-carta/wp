ca5646c415fa33c1e890abdf39caee193119dffb
rosalia-carta <rosalia.carta@gmail.com>
2017-05-26 09:36:54 +0200

    Update index.php

--- wp-content/themes/rose/index.php
+++ wp-content/themes/rose/index.php
@@ -6,8 +6,7 @@
              <h4><?php bloginfo('description')?></h4>
          </div>
      </div>
-  </div>
- </div>
+  
  
  <div class="container-fluid" id="contat">
        <div class="row text-center">
@@ -19,12 +18,32 @@
            <!--start contats -->
            <div class="col-md-6">
             <div class="form-group">
-             <label for="exampleInputEmail1">Indirizzo Email</label>
-             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please type yuor Email" required>
+             
+
+             <!--<label for="exampleInputEmail1">Indirizzo Email</label>
+             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please type yuor Email" required>-->
+
+ <label for="exampleInputEmail1">Indirizzo email</label>
+                    <div class="input-group">
+                      <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
+                      <input name="email" type="email" class="form-control input-lg" id="exampleInputEmail1" placeholder="Digita qui la tua email" required>
+                  </div>
+
+
+
            </div>
            <div class="form-group">
-             <label for="exampleInputPassword1">Password</label>
-             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Choose your Password" required>
+           <!--  <label for="exampleInputPassword1">Password</label>
+             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Choose your Password" required>-->
+
+ <label for="exampleInputTelefono">Telefono</label>
+                 <div class="input-group">
+                  <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-phone-alt"></i></span>
+                  <input name="telefono" type="number" class="form-control input-lg" placeholder="Digita qui il tuo numero di telefono" required>
+              </div>
+
+
+             
            </div>
          </div>
          <div class="col-md-6">
