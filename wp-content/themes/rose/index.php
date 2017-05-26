68ad19ed69df236733571a29a488d1f171f96947
rosalia-carta <rosalia.carta@gmail.com>
2017-05-26 10:50:57 +0200

    Update index.php

--- wp-content/themes/rose/index.php
+++ wp-content/themes/rose/index.php
@@ -1,60 +1,58 @@
   
     <?php get_header();?> 
-    <div class="row">
-            <div class="col-md-12 welcome">
-             <h2><?php bloginfo('name')?></h2>
-             <h4><?php bloginfo('description')?></h4>
-         </div>
-     </div>
-  
- 
- <div class="container-fluid" id="contat">
-       <div class="row text-center">
-         <h1>Contattaci</h1>
-         <h3>e ti risponderemo al più presto</h3>
-       </div>
-       <form class="form-contats">
-         <div class="row">
-           <!--start contats -->
-           <div class="col-md-6">
-            <div class="form-group">
-             
+ <div class="row">
+  <div class="col-md-12 welcome">
+   <h2><?php bloginfo('name')?></h2>
+   <h4><?php bloginfo('description')?></h4>
+ </div>
+</div>
+<div class="container-fluid" id="contat">
+ <div class="row text-center">
+   <h1>Contattaci</h1>
+   <h3>e ti risponderemo al più presto</h3>
+ </div>
+ <form class="form-contats">
+   <div class="row">
+     <!--start contats -->
+     <div class="col-md-6">
+      <div class="form-group">
 
-           <label for="exampleInputEmail1">Indirizzo Email</label>
-             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please type yuor Email" required>
+
+        <label for="exampleInputEmail1">Indirizzo Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Please type yuor Email" required>
 
 <!-- <label for="exampleInputEmail1">Indirizzo email</label>
                     <div class="input-group">
                       <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
                       <input name="email" type="email" class="form-control input-lg" id="exampleInputEmail1" placeholder="Digita qui la tua email" required>
-                  </div>-->
+                    </div>-->
 
 
 
-           </div>
-           <div class="form-group">
-           <label for="exampleInputTelefono">Telefono</label>
-            <input name="telefono" type="number" class="form-control input-lg" placeholder="Digita qui il tuo numero di telefono" required>
+                  </div>
+                  <div class="form-group">
+           <label for="exampleInputPassword1">Password</label>
+           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Choose your Password" required>
 
  <!--<label for="exampleInputTelefono">Telefono</label>
                  <div class="input-group">
                   <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-phone-alt"></i></span>
                   <input name="telefono" type="number" class="form-control input-lg" placeholder="Digita qui il tuo numero di telefono" required>
-              </div>-->
+                </div>-->
 
 
-             
+
+              </div>
+            </div>
+            <div class="col-md-6">
+             <label for="exampleInputmessage">Messaggio</label>
+             <textarea class="form-control" rows="3"></textarea>
            </div>
+
          </div>
-         <div class="col-md-6">
-           <label for="exampleInputmessage">Messaggio</label>
-           <textarea class="form-control" rows="3"></textarea>
+         <div class="row text-center">
+           <button type="submit" class="btn btn-default">Invio</button>
          </div>
-         <!--end contats-->
-       </div>
-       <div class="row text-center">
-         <button type="submit" class="btn btn-default">Invio</button>
-       </div>
-     </form>
+       </form>
      </div>
-  <?php get_footer(); ?> 
+     <?php get_footer(); ?> 
